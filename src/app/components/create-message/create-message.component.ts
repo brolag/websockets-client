4c81f04637b7aff94fdbecb736d96dd152bf2832
Alfredo Bonilla <brolag@live.com>
2018-05-18 19:20:42 -0600

    Clean up create message component

--- src/app/components/create-message/create-message.component.ts
+++ src/app/components/create-message/create-message.component.ts
@@ -1,29 +1,23 @@
-import { Component } from '@angular/core';
-import { ChatService } from '../../services/chat/chat.service';
+import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../../services/chat/chat.service';
 
 @Component({
   selector: 'app-create-message',
   templateUrl: './create-message.component.html',
   styleUrls: ['./create-message.component.css']
 })
-export class CreateMessageComponent {
+export class CreateMessageComponent implements OnInit {
 
-  private message = {
-    author: '',
-    message: ''
-  };
+  private message: Message;
 
   constructor(private chatService: ChatService) { }
+
+  ngOnInit() {
+    this.message = { author: '', message: '' };
+  }
 
   sendMsg() {
     this.chatService.sendMessage(this.message);
     this.message.message = '';
   }
-
-  keyDownFunction(event) {
-    if (event.keyCode === 13) {
-      this.sendMsg();
-    }
-  }
-
 }
